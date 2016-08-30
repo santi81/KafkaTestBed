40f31cc031dd9ccba320506cafd12e06857125e9
Subhobrata Dey <sbcd90@gmail.com>
2016-08-30 16:38:32 -0400

    fix for java-scala conversion

--- src/main/scala/com/sap/kafka/consumer/KafkaStreams.scala
+++ src/main/scala/com/sap/kafka/consumer/KafkaStreams.scala
@@ -1,11 +1,12 @@
 package com.sap.kafka.consumer
 
-import java.util.{Locale, Arrays}
+import java.util.{Arrays, Locale}
 
 import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord, ConsumerRecords, KafkaConsumer}
-import org.apache.kafka.common.serialization.{StringSerializer, Serdes, StringDeserializer}
+import org.apache.kafka.common.serialization.{Serdes, StringDeserializer, StringSerializer}
 import org.apache.kafka.streams.StreamsConfig
-import org.apache.kafka.streams.kstream.{ValueMapper, KStream, KStreamBuilder}
+import org.apache.kafka.streams.kstream.{KStream, KStreamBuilder, ValueMapper}
+import scala.collection.JavaConverters._
 
 object KafkaStreams {
 
@@ -24,19 +25,14 @@ object KafkaStreams {
     props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
 
     val builder = new KStreamBuilder()
-    val textLines: KStream[String,String] = builder.stream(kafkaTopic)
+    val textLines: KStream[String, String] = builder.stream(kafkaTopic)
 
-
-
-    val valueMapper = new ValueMapper[String,Iterable[String]]{
-      override def apply(value: String): Iterable[String]  = {
-
-        value.toLowerCase(Locale.getDefault).split(" ").toList
+    val valueMapper = new ValueMapper[String,java.lang.Iterable[String]]{
+      override def apply(value: String): java.lang.Iterable[String]  = {
+        value.toLowerCase(Locale.getDefault).split(" ").toList.asJava
       }
     }
     textLines.flatMapValues[String](valueMapper)
-
-
 
   }
 }
