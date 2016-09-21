1c7738ab9eddd5060ef7ab35168e20995b122e85
Santosh Addanki <santosh.kumar.addanki@sap.com>
2016-09-20 17:37:40 -0700

    Kafka Partitioned Consumer

---
+++ src/main/scala/com/sap/kafka/consumer/KafkaAssignedPartitionedConsumer.scala
@@ -0,0 +1,48 @@
+package com.sap.kafka.consumer
+
+import java.util
+
+import org.apache.kafka.clients.consumer._
+import org.apache.kafka.common.TopicPartition
+
+object KafkaAssignedPartitionedConsumer {
+
+
+  val kafkaTopic = "topic-with-3-partitions"    // command separated list of topics
+  val kafkaBrokers = "10.97.136.161:9092"   // comma separated list of broker:host
+  val schemaRegistryUrl = "http://10.97.136.161:8081" // Schema registry URL
+  val VALUE_SERIALIZATION_FLAG = "value"
+
+  def main(args: Array[String]): Unit = {
+
+    val props = new java.util.HashMap[String, Object]()
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBrokers)
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+      "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+      "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put("group.id", "testgroup1")
+    props.put("enable.auto.commit", "true")
+    props.put("auto.commit.interval.ms", "1000")
+    props.put("session.timeout.ms", "30000")
+
+    val consumer = new KafkaConsumer[String,String](props)
+    val topicPartition = new TopicPartition(kafkaTopic,0)
+    consumer.assign(util.Arrays.asList(topicPartition))
+    try{
+      while(true) {
+        val records: ConsumerRecords[String, String] = consumer.poll(100)
+        val recordIterator: java.util.Iterator[ConsumerRecord[String, String]] = records.iterator()
+
+        while(recordIterator.hasNext)
+        {
+          val currentRecord :ConsumerRecord[String,String] = recordIterator.next()
+          println(currentRecord.partition())
+
+        }
+      }
+    }
+
+  }
+
+  }
