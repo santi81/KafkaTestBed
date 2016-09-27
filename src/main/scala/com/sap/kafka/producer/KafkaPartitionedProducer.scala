8d4406fffcd52d3c90d0f3c09530573a294ca7e8
Santosh Addanki <santosh.kumar.addanki@sap.com>
2016-09-27 17:07:50 +0800

    Kafka Producer auto-create topic with multiple partitions

--- src/main/scala/com/sap/kafka/producer/KafkaPartitionedProducer.scala
+++ src/main/scala/com/sap/kafka/producer/KafkaPartitionedProducer.scala
@@ -1,14 +1,19 @@
 package com.sap.kafka.producer
 
+
+import java.util.Properties
+
+import kafka.utils.ZkUtils
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
-
 import scala.util.Random
+import kafka.admin.AdminUtils
 
-object KafkaPartitionedProducerDemo {
 
-  val kafkaTopic = "topic-with-3-partitions"    // command separated list of topics
-  val kafkaBrokers = "10.97.136.161:9092"   // comma separated list of broker:host
-  //val kafkaBrokers = "172.16.204.11:6667,172.16.204.14:6667,172.16.204.15:6667,172.16.204.16:6667"   // comma separated list of broker:host
+object KafkaPartitionedProducer {
+
+  val kafkaTopic = "attendee00-partitioned-topic"    // command separated list of topics
+  val kafkaBrokers = "10.97.183.115:9092,10.97.191.51:9092,10.97.152.59:9092,10.97.152.66:9092"
+     // comma separated list of broker:host
 
   // === Configurations of amount of data to produce ===
   val recordsPerSecond = 100000
@@ -16,6 +21,9 @@ object KafkaPartitionedProducerDemo {
   val numSecondsToSend = 120
   val randomWords = List("Germany", "India", "USA")
   val totals = scala.collection.mutable.Map[String, Int]()
+
+  val numPartitions = 3
+  val iSR = 2
 
 
   def main(args: Array[String]): Unit = {
@@ -25,9 +33,24 @@ object KafkaPartitionedProducerDemo {
       "org.apache.kafka.common.serialization.StringSerializer")
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
       "org.apache.kafka.common.serialization.StringSerializer")
+
+    // Specify the partitioner
+
     props.put(ProducerConfig.PARTITIONER_CLASS_CONFIG,
       "com.sap.kafka.partitioner.SimplePartitioner")
 
+
+    // Topic doesnt exist.Though auto.create is enabled it by default it creates 1 partition.
+    // Here we try to directly create a topic by using kafkaCreateTopic
+    try {
+      AdminUtils.createTopic(ZkUtils.apply("10.97.183.115:2181", 10000, 10000, isZkSecurityEnabled = false)
+        , kafkaTopic, numPartitions, iSR, new Properties())
+    }
+    catch {
+      case ex : Exception =>
+        println("Exception creating topic.Terminating")
+        System.exit(1)
+    }
 
 
     println(s"Putting records onto Kafka topic $kafkaTopic at a rate of" +
