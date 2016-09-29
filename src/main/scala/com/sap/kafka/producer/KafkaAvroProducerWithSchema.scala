5889b7e3cd1ec3a15bee928be7375a5924b63670
Santosh Addanki <santosh.kumar.addanki@sap.com>
2016-09-29 11:08:24 +0800

    Refactoring the Avro Producer

---
+++ src/main/scala/com/sap/kafka/producer/KafkaAvroProducerWithSchema.scala
@@ -0,0 +1,46 @@
+package com.sap.kafka.producer
+
+import io.confluent.kafka.serializers.KafkaAvroSerializer
+import org.apache.avro.Schema
+import org.apache.avro.generic.GenericData.Record
+import org.apache.avro.generic.{GenericData, GenericRecord}
+import org.apache.kafka.clients.producer._
+import org.apache.avro.file.DataFileReader
+import scala.io.Source
+
+object KafkaAvroProducerWithSchema {
+
+  val kafkaTopic = "attendee00-kafka-avro-registry"    // command separated list of topics
+  val kafkaBrokers = "10.97.136.161:9092"   // comma separated list of broker:host
+
+  def main(args: Array[String]): Unit = {
+    val props = new java.util.HashMap[String, Object]()
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBrokers)
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+      classOf[KafkaAvroSerializer])
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+      classOf[KafkaAvroSerializer])
+    props.put("schema.registry.url", "http://10.97.136.161:8081")
+
+
+    val fileStream = getClass.getResourceAsStream("/MyRecord.avsc")
+    val schemaFromFile = Source.fromInputStream(fileStream).getLines().mkString
+    val schemaParser = new Schema.Parser
+    val avroSchema = schemaParser.parse(schemaFromFile)
+    val avroProducer = new KafkaProducer[GenericRecord, GenericRecord](props)
+
+    // Create the Avro objects for the key and value
+    val avroRecord = new GenericData.Record(avroSchema)
+    avroRecord.put("str1","field1")
+    avroRecord.put("str2","field2")
+
+    val record = new ProducerRecord[GenericRecord, GenericRecord](kafkaTopic, null, avroRecord )
+    avroProducer.send(record,new Callback {
+      override def onCompletion(metadata: RecordMetadata, exception: Exception): Unit = {
+        // println(s"Message has been published to ${metadata.partition()}")
+        println(s"""The message offset is ${metadata.offset()}""")
+      }
+    })
+    avroProducer.close()
+  }
+}
